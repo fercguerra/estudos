5c121ed7fee41fd4d3dadd037ed320995db1270b
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-09-06 21:09:36 -0300

    ElevatedButton

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+              return Colors.red;
+            }),
+          ),
+          onPressed: () {
+            debugPrint('Clicado');
+          },
+          child: const Text('Meu botão'),
+        ),
+      ),
+    );
+  }
+}
